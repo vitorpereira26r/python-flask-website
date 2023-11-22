dcb7ed7f06fbd6d07d5f382d7827a939c0b8ebef
Vitor Pereira Resende <vitorpereiraresende26@gmail.com>
2023-11-22 14:11:53 -0300

    user account created

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,10 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 
 from .models import User
 
 from werkzeug.security import generate_password_hash, check_password_hash
+
+from . import db
 
 auth = Blueprint('auth', __name__)
 
@@ -33,7 +35,13 @@ def sign_up():
         elif len(password1) < 7:
             flash('Password must be greater than 6 characters.', category='error')
         else:
-            new_user = User(email=email, firstName=firstName, password=password1)
+            new_user = User(email=email, first_name=firstName, password=generate_password_hash(password1, method='pbkdf2:sha256'))
+
+            db.session.add(new_user)
+            db.session.commit()
+
             flash('Account created.', category='success')
+
+            return redirect(url_for('views.home'))
 
     return render_template('sign_up.html')
