949088b6583534858df28b31b87bf5182dc7d91d
Vitor Pereira Resende <vitorpereiraresende26@gmail.com>
2023-11-22 14:33:08 -0300

    checking if there is a user logged in

--- website/auth.py
+++ website/auth.py
@@ -28,7 +28,7 @@ def login():
         else:
             flash("Email does not exist.", category="error")
     
-    return render_template('login.html')
+    return render_template('login.html', user=current_user)
 
 @auth.route('/logout')
 @login_required
@@ -68,4 +68,4 @@ def sign_up():
 
             return redirect(url_for('views.home'))
 
-    return render_template('sign_up.html')
+    return render_template('sign_up.html', user=current_user)
